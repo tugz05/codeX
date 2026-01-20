0609d29e6419691a5dc63c94618866e1ecf40631
Virgilio F Tuga Jr <vftugajr@nemsu.edu.ph>
2026-01-20 14:33:53 +0800

    fix db

---
+++ resources/js/pages/Instructor/Submissions/BatchOperations.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// Batch operations component for instructor submissions
+// This component can be used for bulk operations on submissions
+</script>
+
+<template>
+  <div>
+    <!-- Batch operations component placeholder -->
+  </div>
+</template>
